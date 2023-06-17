953e0bd6fe0a5e7ecb2f9324631a74163f4409b4
nv-cuong <[hanglong01chuong@gmail.com]>
2023-06-17 16:48:01 +0700

    layouts

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('admin/admin/list', function () {
+    return view('admin.admin.list');
+});
